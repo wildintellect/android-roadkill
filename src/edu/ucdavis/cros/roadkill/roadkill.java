de367f7c625b5c34329039f038ebffe36e082e48
Alex Mandel <tech@wildintellect.com>
2011-06-03 00:13:09 -0700

    Added Licensing information. Modified version number to 0.1 may require manual un-install from previous test installations.

--- src/edu/ucdavis/cros/roadkill/roadkill.java
+++ src/edu/ucdavis/cros/roadkill/roadkill.java
@@ -1,4 +1,20 @@
-package edu.ucdavis.cros.roadkill;
+/* Alex Mandel and Paul Haverkamp 
+ * Copyright 2011
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 /* TODO: Add license information, properly cite the Android dev code under Apache 2.0 license
 *
@@ -7,6 +23,9 @@ package edu.ucdavis.cros.roadkill;
 *http://developer.android.com/resources/tutorials/views/hello-datepicker.html
 *
 */
+
+package edu.ucdavis.cros.roadkill;
+
 import java.util.ArrayList;
 import java.util.Calendar;
 import android.app.Activity;
